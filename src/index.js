5e1bdba1c47fe25c84a8a309fbdd65d96267dd65
BarikQ <yar.barik99@gmail.com>
2019-03-09 22:58:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var L = str.length + 1;
+  var reg = /\(\)|\|\||\[\]|\{\}|[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/;
+  while(str = str.replace(reg, '')){
+    console.log(str);
+    if(str.length == L && L != 0) return false;
+    if(L == 0) return true;
+    L = str.length;
+  }
+  return true;
 }
