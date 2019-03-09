f5bc3931062e560c8a0c1824bda0f9b592b09a9c
BarikQ <yar.barik99@gmail.com>
2019-03-09 22:59:34 +0300

    2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ module.exports = function check(str, bracketsConfig) {
   var L = str.length + 1;
   var reg = /\(\)|\|\||\[\]|\{\}|[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/;
   while(str = str.replace(reg, '')){
-    console.log(str);
-    if(str.length == L && L != 0) return false;
     if(L == 0) return true;
+    if(L != 0 && str.length == L) return false; 
     L = str.length;
   }
   return true;
